bef7efec1c40af23c03baf01ff3d69b9b44fdeba
github_username_here <email@example.com>
2025-02-07 12:14:37 +0500

    added player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -13,12 +14,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
